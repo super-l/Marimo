989886c45270737de0df619b868195e0fe72bcdd
lhlsec <lhlsec@gmail.com>
2017-03-31 21:27:49 +0800

    update

--- lib/crawler/common.py
+++ lib/crawler/common.py
@@ -2,7 +2,7 @@ __author__ = 'luhaoliang'
 
 import requests
 
-def download(url,user_agent='wssp'):
+def download(url,user_agent='Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0'):
     headers = {'User-agent':user_agent}
     try:
         html = requests.get(url,headers=headers).text
